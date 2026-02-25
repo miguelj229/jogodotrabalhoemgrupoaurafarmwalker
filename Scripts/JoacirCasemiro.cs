7a5f3ca8200673f9ad95f249406ccfffd3a4596f
JOÃO MIGUEL DORNEL DUTRA SANTOS <joao_md_santos@estudante.sesisenai.org.br>
2026-02-25 16:30:55 -0300

    criação do projeto

---
+++ Scripts/JoacirCasemiro.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class JoacirCasemiro : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Ande para os lados com A+D e pule com espaço!");
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+
+
+
+    }
+}
